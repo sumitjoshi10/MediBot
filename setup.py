d7157f99520c6f7f4912c62a5ca9fc3be56b2f4a
Sumit Joshi <sumit.joshi9818@gmail.com>
2024-10-20 23:08:11 -0400

    Completion of setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="MediBot",
+    version="0.0.1",
+    author="Sumit Joshi",
+    author_email="sumit.joshi9818@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    
+)
